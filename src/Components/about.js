1019305d550ea9ac01c4b793c2a2ab98841f34b6
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-06-02 16:13:24 +0530

    book-assessment

--- src/Components/about.js
+++ src/Components/about.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const About = ()=>{
     return(
-        <h1>Hello 3</h1>
+      <div className='container'>
+           <div className='row aboutDiv'>
+           <h2 className='text text-success' style={{textAlign:'center'}}>Our Books</h2>
+           <div className='col-sm-6'>
+           <img src='http://localhost:8090/book20.jpg'/>
+           </div>
+           <div className='col-sm-6'>
+           <img src='http://localhost:8090/book25.jpg'/>
+           </div>
+       </div>
+      </div>
     )
 }
 
