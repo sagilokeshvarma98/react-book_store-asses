241a206ed6ae06fe021a25ab9f88d244a15f2872
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-06-02 15:51:03 +0530

    contact-commit

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -4,13 +4,19 @@ const Contact = ()=>{
     return(
        <div>
            <h2 className='text text-danger' style={{textAlign:'center'}}>Contact Us</h2>
-           <div className='container' style={{margin:'50px'}}>
+           <div className='container contactDiv' style={{margin:'50px'}}>
                <h5>Vist below mentioned address</h5>
                <ul style={{listStyle:'none'}}>
                    <li>HouseNo : 2/23</li>
                    <li>Bandari Layout</li>
                    <li>NIzampet , Hyderabad</li>
                    <li>500090</li>
+               </ul>
+               <ul style={{listStyle:'none'}}>
+                   <li>Flat no : 203</li>
+                   <li>Near Sai baba Temple</li>
+                   <li>dammaiguda , Secunderabad</li>
+                   <li>500083</li>
                </ul>
            </div>
        </div>
